488ef1823e828e8920055bcdfb52a9ab2fda11df
chandu <chan30du@gmail.com>
2022-11-27 03:30:04 +0530

    Final

--- server.py
+++ server.py
@@ -288,4 +288,4 @@ def errors():
     return render_template('error.html')
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=env.get("PORT", 3000), debug=True)
+    app.run(debug=False)
